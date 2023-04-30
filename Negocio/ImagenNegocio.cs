b2f91fef06289b1644d5c45f8dcf3f8e6249765d
dmatia <diegovillalba.96@gmail.com>
2023-04-30 18:43:08 -0300

    ImagenNegocio - listar

--- Negocio/ImagenNegocio.cs
+++ Negocio/ImagenNegocio.cs
@@ -1,8 +1,47 @@
-﻿using System;
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
 
-public class Class1
+namespace Negocio
 {
-	public Class1()
-	{
-	}
+    public class ImagenNegocio
+    {
+
+        // METODO LISTAR - Busca por articulo 
+        public List<Imagen> listar(string IdArticulo)
+        {
+            List<Imagen> lista = new List<Imagen>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setConsulta("SELECT Id, IdArticulo, ImagenUrl FROM Imagenes WHERE IdArticulo = " + IdArticulo);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Imagen aux = new Imagen();
+                    aux.Id = (Int32)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
 }
