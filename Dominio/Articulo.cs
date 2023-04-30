641b4425a2b74fc4066f6daffc8abf44a9e8803e
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-04-30 18:10:32 -0300

    Modifica clases a públicas

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -7,23 +7,26 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Articulo
+    public class Articulo
     {
-        public int Id { get; }
+        public int Id { get; set; }
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public Double Precio { get; set; }
+		public List<Imagen> Imagenes { get; set; }
 
-        public Articulo{
-            this.Id = "";
+		public Articulo()
+        {
+            this.Id = 0;
             this.Codigo = "";
             this.Nombre = "";
             this.Descripcion = "";
             this.Marca = new Marca();
             this.Categoria = new Categoria();
-            this.Double = "";
+            this.Precio = 0;
         }
+    }
 }
