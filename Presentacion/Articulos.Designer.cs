73246b9cd82876fcde86ad289064354bf1ca85de
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-05-03 15:59:30 -0300

    deshabilito edicion en lista de articulos

--- Presentacion/Articulos.Designer.cs
+++ Presentacion/Articulos.Designer.cs
@@ -28,140 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.btnAgregar = new System.Windows.Forms.Button();
-            this.btnDetalle = new System.Windows.Forms.Button();
-            this.dgvListaArticulos = new System.Windows.Forms.DataGridView();
-            this.TxtBuqueda = new System.Windows.Forms.TextBox();
-            this.CbxFiltroprimario = new System.Windows.Forms.ComboBox();
-            this.CbxCategoria = new System.Windows.Forms.ComboBox();
-            this.CbxMarca = new System.Windows.Forms.ComboBox();
-            this.BtnBusqueda = new System.Windows.Forms.Button();
-            this.BtnReset = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListaArticulos)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Location = new System.Drawing.Point(183, 369);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
-            this.btnEliminar.TabIndex = 3;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            // 
-            // btnAgregar
-            // 
-            this.btnAgregar.Location = new System.Drawing.Point(102, 369);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
-            this.btnAgregar.TabIndex = 4;
-            this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
-            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
-            // 
-            // btnDetalle
-            // 
-            this.btnDetalle.Location = new System.Drawing.Point(21, 370);
-            this.btnDetalle.Name = "btnDetalle";
-            this.btnDetalle.Size = new System.Drawing.Size(75, 23);
-            this.btnDetalle.TabIndex = 5;
-            this.btnDetalle.Text = "Detalle";
-            this.btnDetalle.UseVisualStyleBackColor = true;
-            this.btnDetalle.Click += new System.EventHandler(this.btnDetalle_Click);
-            // 
-            // dgvListaArticulos
-            // 
-            this.dgvListaArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListaArticulos.Location = new System.Drawing.Point(20, 128);
-            this.dgvListaArticulos.Name = "dgvListaArticulos";
-            this.dgvListaArticulos.RowHeadersWidth = 51;
-            this.dgvListaArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvListaArticulos.Size = new System.Drawing.Size(776, 234);
-            this.dgvListaArticulos.TabIndex = 2;
-            // 
-            // TxtBuqueda
-            // 
-            this.TxtBuqueda.Location = new System.Drawing.Point(23, 88);
-            this.TxtBuqueda.Name = "TxtBuqueda";
-            this.TxtBuqueda.Size = new System.Drawing.Size(273, 20);
-            this.TxtBuqueda.TabIndex = 11;
-            this.TxtBuqueda.Text = "Ingrese busqueda...";
-            this.TxtBuqueda.Click += new System.EventHandler(this.TxtBuqueda_Click);
-            // 
-            // CbxFiltroprimario
-            // 
-            this.CbxFiltroprimario.FormattingEnabled = true;
-            this.CbxFiltroprimario.Location = new System.Drawing.Point(302, 87);
-            this.CbxFiltroprimario.Name = "CbxFiltroprimario";
-            this.CbxFiltroprimario.Size = new System.Drawing.Size(133, 21);
-            this.CbxFiltroprimario.TabIndex = 12;
-            this.CbxFiltroprimario.Text = "Filtros disponibles";
-            // 
-            // CbxCategoria
-            // 
-            this.CbxCategoria.FormattingEnabled = true;
-            this.CbxCategoria.Location = new System.Drawing.Point(441, 87);
-            this.CbxCategoria.Name = "CbxCategoria";
-            this.CbxCategoria.Size = new System.Drawing.Size(133, 21);
-            this.CbxCategoria.TabIndex = 13;
-            this.CbxCategoria.Text = "Categorias";
-            this.CbxCategoria.SelectedIndexChanged += new System.EventHandler(this.CbxCategoria_SelectedIndexChanged);
-            this.CbxCategoria.Click += new System.EventHandler(this.CbxCategoria_Click);
-            // 
-            // CbxMarca
-            // 
-            this.CbxMarca.FormattingEnabled = true;
-            this.CbxMarca.Location = new System.Drawing.Point(580, 87);
-            this.CbxMarca.Name = "CbxMarca";
-            this.CbxMarca.Size = new System.Drawing.Size(133, 21);
-            this.CbxMarca.TabIndex = 14;
-            this.CbxMarca.Text = "Marcas";
-            // 
-            // BtnBusqueda
-            // 
-            this.BtnBusqueda.Location = new System.Drawing.Point(719, 87);
-            this.BtnBusqueda.Name = "BtnBusqueda";
-            this.BtnBusqueda.Size = new System.Drawing.Size(75, 23);
-            this.BtnBusqueda.TabIndex = 15;
-            this.BtnBusqueda.Text = "Buscar";
-            this.BtnBusqueda.UseVisualStyleBackColor = true;
-            this.BtnBusqueda.Click += new System.EventHandler(this.BtnBusqueda_Click);
-            // 
-            // BtnReset
-            // 
-            this.BtnReset.Location = new System.Drawing.Point(622, 368);
-            this.BtnReset.Name = "BtnReset";
-            this.BtnReset.Size = new System.Drawing.Size(174, 25);
-            this.BtnReset.TabIndex = 16;
-            this.BtnReset.Text = "Reestablecer busqueda";
-            this.BtnReset.UseVisualStyleBackColor = true;
-            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
-            // 
-            // Articulos
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 578);
-            this.ControlBox = false;
-            this.Controls.Add(this.BtnReset);
-            this.Controls.Add(this.BtnBusqueda);
-            this.Controls.Add(this.CbxMarca);
-            this.Controls.Add(this.CbxCategoria);
-            this.Controls.Add(this.CbxFiltroprimario);
-            this.Controls.Add(this.TxtBuqueda);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.btnDetalle);
-            this.Controls.Add(this.dgvListaArticulos);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "Articulos";
-            this.Text = "Articulos";
-            this.Load += new System.EventHandler(this.Articulos_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListaArticulos)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.btnEliminar = new System.Windows.Forms.Button();
+			this.btnAgregar = new System.Windows.Forms.Button();
+			this.btnDetalle = new System.Windows.Forms.Button();
+			this.dgvListaArticulos = new System.Windows.Forms.DataGridView();
+			this.TxtBuqueda = new System.Windows.Forms.TextBox();
+			this.CbxFiltroprimario = new System.Windows.Forms.ComboBox();
+			this.CbxCategoria = new System.Windows.Forms.ComboBox();
+			this.CbxMarca = new System.Windows.Forms.ComboBox();
+			this.BtnBusqueda = new System.Windows.Forms.Button();
+			this.BtnReset = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgvListaArticulos)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnEliminar
+			// 
+			this.btnEliminar.Location = new System.Drawing.Point(183, 369);
+			this.btnEliminar.Name = "btnEliminar";
+			this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+			this.btnEliminar.TabIndex = 3;
+			this.btnEliminar.Text = "Eliminar";
+			this.btnEliminar.UseVisualStyleBackColor = true;
+			// 
+			// btnAgregar
+			// 
+			this.btnAgregar.Location = new System.Drawing.Point(102, 369);
+			this.btnAgregar.Name = "btnAgregar";
+			this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+			this.btnAgregar.TabIndex = 4;
+			this.btnAgregar.Text = "Agregar";
+			this.btnAgregar.UseVisualStyleBackColor = true;
+			this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+			// 
+			// btnDetalle
+			// 
+			this.btnDetalle.Location = new System.Drawing.Point(21, 370);
+			this.btnDetalle.Name = "btnDetalle";
+			this.btnDetalle.Size = new System.Drawing.Size(75, 23);
+			this.btnDetalle.TabIndex = 5;
+			this.btnDetalle.Text = "Detalle";
+			this.btnDetalle.UseVisualStyleBackColor = true;
+			this.btnDetalle.Click += new System.EventHandler(this.btnDetalle_Click);
+			// 
+			// dgvListaArticulos
+			// 
+			this.dgvListaArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvListaArticulos.Location = new System.Drawing.Point(20, 128);
+			this.dgvListaArticulos.Name = "dgvListaArticulos";
+			this.dgvListaArticulos.ReadOnly = true;
+			this.dgvListaArticulos.RowHeadersWidth = 51;
+			this.dgvListaArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dgvListaArticulos.Size = new System.Drawing.Size(776, 234);
+			this.dgvListaArticulos.TabIndex = 2;
+			// 
+			// TxtBuqueda
+			// 
+			this.TxtBuqueda.Location = new System.Drawing.Point(23, 88);
+			this.TxtBuqueda.Name = "TxtBuqueda";
+			this.TxtBuqueda.Size = new System.Drawing.Size(273, 20);
+			this.TxtBuqueda.TabIndex = 11;
+			this.TxtBuqueda.Text = "Ingrese busqueda...";
+			this.TxtBuqueda.Click += new System.EventHandler(this.TxtBuqueda_Click);
+			// 
+			// CbxFiltroprimario
+			// 
+			this.CbxFiltroprimario.FormattingEnabled = true;
+			this.CbxFiltroprimario.Location = new System.Drawing.Point(302, 87);
+			this.CbxFiltroprimario.Name = "CbxFiltroprimario";
+			this.CbxFiltroprimario.Size = new System.Drawing.Size(133, 21);
+			this.CbxFiltroprimario.TabIndex = 12;
+			this.CbxFiltroprimario.Text = "Filtros disponibles";
+			// 
+			// CbxCategoria
+			// 
+			this.CbxCategoria.FormattingEnabled = true;
+			this.CbxCategoria.Location = new System.Drawing.Point(441, 87);
+			this.CbxCategoria.Name = "CbxCategoria";
+			this.CbxCategoria.Size = new System.Drawing.Size(133, 21);
+			this.CbxCategoria.TabIndex = 13;
+			this.CbxCategoria.Text = "Categorias";
+			this.CbxCategoria.SelectedIndexChanged += new System.EventHandler(this.CbxCategoria_SelectedIndexChanged);
+			this.CbxCategoria.Click += new System.EventHandler(this.CbxCategoria_Click);
+			// 
+			// CbxMarca
+			// 
+			this.CbxMarca.FormattingEnabled = true;
+			this.CbxMarca.Location = new System.Drawing.Point(580, 87);
+			this.CbxMarca.Name = "CbxMarca";
+			this.CbxMarca.Size = new System.Drawing.Size(133, 21);
+			this.CbxMarca.TabIndex = 14;
+			this.CbxMarca.Text = "Marcas";
+			// 
+			// BtnBusqueda
+			// 
+			this.BtnBusqueda.Location = new System.Drawing.Point(719, 87);
+			this.BtnBusqueda.Name = "BtnBusqueda";
+			this.BtnBusqueda.Size = new System.Drawing.Size(75, 23);
+			this.BtnBusqueda.TabIndex = 15;
+			this.BtnBusqueda.Text = "Buscar";
+			this.BtnBusqueda.UseVisualStyleBackColor = true;
+			this.BtnBusqueda.Click += new System.EventHandler(this.BtnBusqueda_Click);
+			// 
+			// BtnReset
+			// 
+			this.BtnReset.Location = new System.Drawing.Point(622, 368);
+			this.BtnReset.Name = "BtnReset";
+			this.BtnReset.Size = new System.Drawing.Size(174, 25);
+			this.BtnReset.TabIndex = 16;
+			this.BtnReset.Text = "Reestablecer busqueda";
+			this.BtnReset.UseVisualStyleBackColor = true;
+			this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+			// 
+			// Articulos
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(836, 578);
+			this.ControlBox = false;
+			this.Controls.Add(this.BtnReset);
+			this.Controls.Add(this.BtnBusqueda);
+			this.Controls.Add(this.CbxMarca);
+			this.Controls.Add(this.CbxCategoria);
+			this.Controls.Add(this.CbxFiltroprimario);
+			this.Controls.Add(this.TxtBuqueda);
+			this.Controls.Add(this.btnEliminar);
+			this.Controls.Add(this.btnAgregar);
+			this.Controls.Add(this.btnDetalle);
+			this.Controls.Add(this.dgvListaArticulos);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "Articulos";
+			this.Text = "Articulos";
+			this.Load += new System.EventHandler(this.Articulos_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dgvListaArticulos)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
